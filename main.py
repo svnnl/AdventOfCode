b1a878e4ef04c683064b773c9cba37ed96dac550
SvNNL <sebastiaanvannijen@hotmail.com>
2021-12-01 18:23:04 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from sln.advent_1 import sol1
+
+def run_solution(option):
+    print(option)
+
+def menu():
+    print('Select the number of the Advent code you want to run:')
+    for file in os.listdir('sln'):
+        print(file)
+    option = input()
+    os.system('cls')
+
+    run_solution(option)
+
+
+if __name__ == '__main__':
+    sol1()
+
---
+++ sln/advent_1.py
@@ -0,0 +1,12 @@
+def sol1():
+    file = open('data/advent_1.txt').read()
+    numbers = file.splitlines()
+
+    count = 0
+    for i in range(0, len(numbers) - 1):
+        current = int(numbers[i])
+        next = int(numbers[i+1])
+        if next > current:
+            count += 1
+
+    print(count)
