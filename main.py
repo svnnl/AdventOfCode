f702e6876f6da43e95394e0dff0a5673068f4f24
SvN <sebastiaanvannijen@hotmail.com>
2021-12-03 11:00:03 +0100

    Day 3 part 1 finished

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import os
 import PySimpleGUI as sg
 from sln.advent_1 import sol1
 from sln.advent_2 import sol2
-
+from sln.advent_3 import sol3
 
 def run_solution(option):
     if option == '1':
         answer = sol1()
     elif option == '2':
         answer = sol2()
+    elif option == '3':
+        answer = sol3()
 
     for i in range(0, len(answer)):
         print("Solution of Day " + option + " - Part " + str(i + 1) + ": " + str(answer[i]))
@@ -26,5 +28,5 @@ def menu():
 
 
 if __name__ == '__main__':
-    sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+    #sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
     menu()
--- sln/advent_3.py
+++ sln/advent_3.py
@@ -1,4 +1,23 @@
+import numpy as np
+from scipy import stats
+
+
 def sol3():
     answers = []
+
+    # PART 1
+    file = np.loadtxt("data/advent_3.txt", dtype=str)
+    binary_array = np.array([list(i) for i in file])
+
+    modes = []
+
+    for i in range(0, 12):
+        mode = stats.mode([item[i] for item in binary_array])[0]
+        modes.append(int(mode))
+
+    gamma = ''.join(map(str, modes))
+    epsilon = ''.join(['1' if i == '0' else '0' for i in gamma])
+
+    answers.append(int(gamma, 2) * int(epsilon, 2))
 
     return answers
