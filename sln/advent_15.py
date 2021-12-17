d87127dc0136f660cd95af735978363439d2c49f
SvN <sebastiaanvannijen@hotmail.com>
2021-12-17 16:07:36 +0100

    15 continuation

--- sln/advent_15.py
+++ sln/advent_15.py
@@ -10,10 +10,52 @@ np.set_printoptions(linewidth=len(data[0]))
 print(data)
 
 
-def neighbours(node):
-    pass
+def adj(node):
+    i = node[0]
+    j = node[1]
+
+    n = len(data[0])
+    m = len(data)
+
+    adjacent_indices = []
+
+    # North
+    # if i > 0:
+    #    adjacent_indices.append((i - 1, j))
+
+    # South
+    if i + 1 < m:
+        adjacent_indices.append((i + 1, j))
+
+    # West
+    # if j > 0:
+    #    adjacent_indices.append((i, j - 1))
+
+    # East
+    if j + 1 < n:
+        adjacent_indices.append((i, j + 1))
+
+    return adjacent_indices
 
 
-def find_shortest_path(graph, visited):
-    shortest_path = []
-    pass
+def find_shortest_path(graph, start):
+    shortest_path = {}
+    unvisited = {}
+
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            unvisited.add((i, j))
+
+    pq = PriorityQueue()
+
+    neighbours = adj(start)
+
+    for neighbours in neighbours:
+        continue
+
+    print(neighbours)
+
+    print(unvisited)
+
+
+find_shortest_path(data, (0, 0))
