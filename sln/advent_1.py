afe56139b385715f757c42c041e6b8f4562fb0eb
SvNNL <sebastiaanvannijen@hotmail.com>
2022-01-03 16:14:49 +0100

    1 done

--- sln/advent_1.py
+++ sln/advent_1.py
@@ -1,5 +1,11 @@
-def sol1():
-    return [a * b for a, b in data]
+with open('../data/advent_1.txt') as f:
+    data = list(map(int, f.read().splitlines()))
 
+print(data)
 
-sol1()
+for i in range(0, len(data) - 1):
+    for j in range(i + 1, len(data)):
+        for k in range(j + 1, len(data)):
+            if data[i] + data[j] + data[k] == 2020:
+                print('{0} + {1} + {2} = {3}'.format(data[i], data[j], data[k], data[i] + data[j] + data[k]))
+                print(data[i] * data[j] * data[k])
