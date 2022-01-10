4f93d541c54cb6c5a57cd81aaf787561798f208d
SvNNL <sebastiaanvannijen@hotmail.com>
2022-01-10 12:30:56 +0100

    update 13-2

--- sln/advent_13.py
+++ sln/advent_13.py
@@ -6,7 +6,6 @@ with open('../data/advent_13.txt') as f:
     value, busses = f.read().splitlines()
 
 bus_ids = [int(x) for x in busses.split(',') if x != 'x']
-print(bus_ids)
 
 earliest_bus = 0
 best_diff = 1
@@ -15,19 +14,17 @@ best_rounding_diff = 1
 for i in bus_ids:
     diff = int(value) / i
     rounding_diff = math.ceil(diff) - diff
-    print(diff, rounding_diff)
     if rounding_diff < best_rounding_diff:
         earliest_bus = i
         best_diff = diff
         best_rounding_diff = rounding_diff
 
-print('Answer to Part 1: {}'.format(earliest_bus * ((math.ceil(best_diff) * earliest_bus) - int(value))))
+print(f'Answer to Part 1: {earliest_bus * ((math.ceil(best_diff) * earliest_bus) - int(value))}')
 
 # Part 2
 
 busses = busses.split(',')
 busses = [(int(busses[k]), k) for k in range(len(busses)) if busses[k] != 'x']
-print(busses)
 
 multiplier = 1
 start = 0
@@ -38,4 +35,4 @@ for i in range(len(busses) - 1):
     while (start + position) % bus_id != 0:
         start += multiplier
 
-print('Answer to Part 2: {}'.format(start))
+print(f'Answer to Part 2: {start}')
