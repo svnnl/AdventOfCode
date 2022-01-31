a5177c7396ef3adf279b01edb4bb6be50d4cbb58
SvNNL <sebastiaanvannijen@hotmail.com>
2022-01-31 23:42:50 +0100

    1-2

--- sln/advent_1.py
+++ sln/advent_1.py
@@ -1,4 +1,14 @@
 import math
 
 with open('../data/advent_1.txt') as f:
-    print(sum([math.floor(i / 3) - 2 for i in list(map(int, f.read().splitlines()))]))
+    data = list(map(int, f.read().splitlines()))
+
+print(f'Answer to Part 1: {sum([math.floor(i / 3) - 2 for i in data])}')
+
+cost = 0
+for i in data:
+    while math.floor(i / 3) - 2 > 0:
+        i = math.floor(i / 3) - 2
+        cost += i
+
+print(f'Answer to Part 2: {cost}')
