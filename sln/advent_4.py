cb4304157d5a1fe8c7f7af79aff0c95396ebf178
SvNNL <sebastiaanvannijen@hotmail.com>
2022-09-04 17:17:26 +0200

    4,5,6,7

---
+++ sln/advent_4.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+with open('../data/advent_4.txt') as f:
+    data = [i.split(' ') for i in f.read().splitlines()]
+
+res = 0
+
+for pw in data:
+    valid = True
+    occ = defaultdict(int)
+    for word in pw:
+        if word in occ:
+            valid = False
+        else:
+            occ[word] += 1
+    if valid:
+        res += 1
+
+print(f'Answer to Part 1: {res}')
+
+res = 0
+
+for pw in data:
+    valid = True
+    occ = defaultdict(int)
+    for word in pw:
+        if ''.join(sorted(word)) in occ:
+            valid = False
+        else:
+            occ[''.join(sorted(word))] += 1
+    if valid:
+        res += 1
+
+print(f'Answer to Part 2: {res}')
---
+++ sln/advent_5.py
@@ -0,0 +1,33 @@
+with open('../data/advent_5.txt') as f:
+    data = list(map(int, f.read().splitlines()))
+
+steps = 0
+index = 0
+instr = data.copy()
+
+while True:
+    try:
+        steps += 1
+        prev = index
+        index += instr[index]
+        if instr[prev] < 3:
+            instr[prev] += 1
+        else:
+            instr[prev] -= 1
+    except IndexError:
+        print(f'Answer to Part 2: {steps - 1}')
+        break
+
+steps = 0
+index = 0
+instr = data.copy()
+
+while True:
+    try:
+        steps += 1
+        prev = index
+        index += data[index]
+        data[prev] += 1
+    except IndexError:
+        print(f'Answer to Part 1: {steps - 1}')
+        break
