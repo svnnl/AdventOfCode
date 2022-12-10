cdf6a579a8be708657aede9d3f2d3707bae8d3da
SvNNL <sebastiaanvannijen@hotmail.com>
2022-12-11 00:06:38 +0100

    Quick fix for some faulty pixels

--- sln/advent_10.py
+++ sln/advent_10.py
@@ -3,7 +3,7 @@ import numpy as np
 data = open('../data/advent_10.txt').read().splitlines()
 
 register = 1
-checkpoints = []
+signal = 0
 cycle = 0
 executing = False
 i = 0
@@ -12,26 +12,26 @@ drawing = []
 
 while cycle < 240:
     cycle += 1
-    position = cycle % 40 - 1
-    if position in range(register - 1, register + 2):
-        drawing.append('#')
+    position = (cycle - 1) % 40
+    if position in [register - 1, register, register + 1]:
+        drawing.append('█')
     else:
-        drawing.append('.')
+        drawing.append(' ')
 
-    if (cycle + 20) % 40 == 0:
-        checkpoints.append(register * cycle)
+    if cycle % 40 == 20:
+        signal += register * cycle
 
-    if data[i] != 'noop':
+    if data[i] == 'noop':
+        i += 1
+    else:
         if executing:
             register += int(data[i].split()[1])
             executing = False
             i += 1
         else:
             executing = True
-    else:
-        i += 1
 
-print(f'Answer to Part 1: {sum(checkpoints)}')
+print(f'Answer to Part 1: {signal}')
 
 drawing = np.array_split(np.asarray(drawing), 6)
 print(*[' '.join(i) for i in drawing], sep='\n')
