b42b6fec48eeb8b133ee289998bdbced69d53bdc
Sebastiaan van Nijen <nijensa@delagelanden.com>
2024-12-02 01:43:17 +0100

    util

---
+++ utils/create_files.sh
@@ -0,0 +1,11 @@
+touch $1/data/aoc_$2.txt
+
+touch $1/test/test_aoc_$2.txt
+
+echo "TEST = 1
+day = __file__.split(\"\\\\\")[-1][:-3]
+
+path = f'2024/test/test_{day}.txt' if TEST else f'2024/data/{day}.txt'
+
+data = open(path, 'r').read()
+print(data)" > $1/sln/aoc_$2.py
